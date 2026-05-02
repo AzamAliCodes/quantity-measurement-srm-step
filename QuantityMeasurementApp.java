d1fd5f842c562d292e2e115e38ede5eb80c19341
AzamAliCodes <social.azamali@gmail.com>
2026-05-03 01:05:34 +0530

    UC2: Feet and Inches measurement equality

--- QuantityMeasurementApp.java
+++ QuantityMeasurementApp.java
@@ -21,18 +21,40 @@ public class QuantityMeasurementApp {
         }
     }
 
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches inches = (Inches) obj;
+            return Double.compare(inches.value, value) == 0;
+        }
+
+        @Override
+        public String toString() {
+            return value + " in";
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Starting UC1: Feet measurement equality");
+        System.out.println("Starting UC2: Feet and Inches measurement equality");
 
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
-        Feet f3 = new Feet(2.0);
-
         System.out.println("Test 1.0 ft == 1.0 ft: " + f1.equals(f2));
-        System.out.println("Test 1.0 ft != 2.0 ft: " + !f1.equals(f3));
-        System.out.println("Test Null Comparison: " + !f1.equals(null));
-        System.out.println("Test Type Safety: " + !f1.equals("1.0"));
 
-        System.out.println("UC1 Verification Complete.");
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+        System.out.println("Test 1.0 in == 1.0 in: " + i1.equals(i2));
+
+        System.out.println("Test 1.0 ft != 1.0 in: " + !f1.equals(i1));
+
+        System.out.println("UC2 Verification Complete.");
     }
 }
